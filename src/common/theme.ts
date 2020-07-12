a5c98748f4e12dcb46d40c1cefc828b80084e8d6
shunke07 <s.takami07@gmail.com>
2020-07-12 23:54:41 +0900

    Add common/theme for style variables

---
+++ src/common/theme.ts
@@ -0,0 +1,22 @@
+const $colors = {
+  primary: {
+    light: "#ff7043",
+    main: "#ff5722",
+  },
+  secondary: {
+    main: "#007c91",
+  },
+  gray: {
+    light: "#616161",
+    main: "#424242",
+  },
+};
+
+const $boxShadows = {
+  "1dp":
+    "0 1px 1px 0 rgba(0, 0, 0, 0.14), 0 2px 1px -1px rgba(0, 0, 0, 0.12), 0 1px 3px 0 rgba(0, 0, 0, 0.2)",
+  "4dp":
+    "0 4px 5px 0 rgba(0, 0, 0, 0.14), 0 1px 10px 0 rgba(0, 0, 0, 0.12), 0 2px 4px -1px rgba(0, 0, 0, 0.2);",
+};
+
+export { $colors, $boxShadows };
