ad3f8a6061311a20cba6f68314b93e52c68163b7
shunke07 <s.takami07@gmail.com>
2020-07-12 20:45:45 +0900

    Add pages/_document

---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render(): JSX.Element {
+    return (
+      <Html lang="ja">
+        <Head>
+          <meta
+            name="description"
+            content="This site is Shunke's website. Shunke is Software Developer, Web Front-End Developer in Tokyo."
+          />
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/icons/apple-touch-icon.png"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
