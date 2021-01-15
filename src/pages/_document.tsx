5ad890244bd034ed7f8ce095e242115ba4ca737e
shunke07 <s.takami07@gmail.com>
2021-01-15 21:06:34 +0900

    Update head meta for pwa, ogp

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,14 +7,58 @@ class MyDocument extends Document {
         <Head>
           <meta
             name="description"
-            content="This site is Shunke's website. Shunke is Software Developer, Web Front-End Developer in Tokyo."
+            content="Shunke is Software Developer, Web Front-End Developer in Tokyo."
           />
+          {/* Web App */}
+          <meta name="application-name" content="shunke07.com" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content="shunke07.com" />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="theme-color" content="#ef6c00" />
           <link
             rel="apple-touch-icon"
             sizes="180x180"
             href="/icons/apple-touch-icon.png"
           />
           <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/icons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/icons/favicon-16x16.png"
+          />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="manifest" href="/static/manifest.json" />
+          {/* OGP */}
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:url" content="https://shunke07.com" />
+          <meta name="twitter:title" content="shunke07.com" />
+          <meta
+            name="twitter:description"
+            content="Shunke is Software Developer, Web Front-End Developer in Tokyo."
+          />
+          <meta name="twitter:image" content="/icons/apple-touch-icon.png" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="shunke07.com" />
+          <meta
+            property="og:description"
+            content="Shunke is Software Developer, Web Front-End Developer in Tokyo."
+          />
+          <meta property="og:site_name" content="shunke07.com" />
+          <meta property="og:url" content="https://shunke07.com" />
+          <meta property="og:image" content="/icons/apple-touch-icon.png" />
+          {/* Web-font */}
           <link
             rel="preload"
             href="/fonts/Montserrat-Bold.ttf"
