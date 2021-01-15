c4b7e995c42a66df8d9515e6fa31062dabc2d3f3
shunke07 <s.takami07@gmail.com>
2021-01-15 21:06:17 +0900

    Add next-pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+import withPWA from "next-pwa";
+
 // eslint-disable-next-line
-module.exports = {
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+  },
   images: {
     domains: ["images.microcms-assets.io"],
   },
-};
+});
