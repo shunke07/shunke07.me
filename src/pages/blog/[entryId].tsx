15c4f92525db0fbcb0e15049b2fb834e233e84e3
shunke07 <s.takami07@gmail.com>
2021-01-16 16:19:27 +0900

    Update entry img width

--- src/pages/blog/[entryId].tsx
+++ src/pages/blog/[entryId].tsx
@@ -127,9 +127,9 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   $("img").each((_, elm) => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const currentSrc = $(elm).attr("src");
-    $(elm).attr("src", () => currentSrc + "?w=680");
-    $(elm).attr("width", () => "680");
-    $(elm).attr("height", () => "340");
+    $(elm).attr("src", () => currentSrc + "?w=720");
+    $(elm).attr("width", () => "720");
+    $(elm).attr("height", () => "320");
     $(elm).attr("loading", () => "lazy");
     $(elm).attr("alt", () => "画像");
   });
